64d4a429ba1ba1fcf6b414c22bbd25dcacc6d6bc
jacegem <jacegem@gmail.com>
2016-06-28 13:35:00 +0900

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+"""`main` is the top level module for your Flask application."""
+
+from google.appengine.ext import ndb
+from flask import jsonify 
+
+# Import the Flask Framework
+from flask import Flask
+
+app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!22'
+
+@app.route('/other')
+def helloOther():
+    """Return a friendly HTTP greeting."""
+    return 'Hello Other World!22'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
